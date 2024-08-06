15e2ff7b769f24eaadf5fb3de14f047d460efda7
AryanChakma <jhonuchakma@gmail.com>
2024-08-07 01:07:59 +0530

    Responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 var elemC = document.querySelector("#elem-container");
 var fixed = document.querySelector("#fixed-image");
 elemC.addEventListener("mouseenter", function(){
